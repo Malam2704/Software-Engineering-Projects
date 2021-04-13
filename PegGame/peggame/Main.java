b6aadb0709e8657998512d4b122a581157960101
Mohammed Alam <ma3655@rit.edu>
2021-04-13 00:15:33 -0400

    UI made

--- PegGame/peggame/Main.java
+++ PegGame/peggame/Main.java
@@ -7,7 +7,7 @@ package peggame;
 
 import java.util.Scanner;
 
-public class Project1Main {
+public class Main {
     // Below is a main function which asks for a filename
     // to start with, the runs an in stance of our game.
     public static void main(String[] args) throws PegGameException{
@@ -16,6 +16,7 @@ public class Project1Main {
             "just like checkers\nNow in the data directory there are many board sizes, so " + 
             "take your pick.\n");
         System.out.println("type 'help' anytime for more information\n");
+        System.out.println("All the board sizes are in the data directory, just enter a filename starting with data/\n");
         Scanner scanner = new Scanner(System.in);
         System.out.print("ENTER FILENAME: ");
         String userInput = scanner.nextLine();
